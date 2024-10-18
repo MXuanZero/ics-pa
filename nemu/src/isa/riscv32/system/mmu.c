7937fd9f650bc8335c4db2d22a1214f6c6f8061f
tracer-ics2024 <tracer@njuics.org>
2024-10-18 10:37:36 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-O70Q5GQ 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:37:36 up  1:44,  1 user,  load average: 0.14, 0.04, 0.01

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -18,31 +18,31 @@
 #include <memory/paddr.h>
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-	uint8_t c_max = 4;
+	uint8_t cols_max = 4; // maximum number of columns
 	word_t word[4];
-	int line = len ? ((len - 1) / c_max) + 1 : 1;
+	int line = len ? ((len - 1) / cols_max) + 1 : 1;
 	int num = len;
 
 	for(int i = 0; i < line; i++) {
 		for(int j = 0; j < 4; j++) {
-			word[j] = vaddr_read(vaddr + i * c_max + j * type, type);
+			word[j] = vaddr_read(vaddr + i * cols_max + j * type, type);
 		}
 
 		switch (num) {
 		case 1:
-			printf("0x%.8x: 0x%.8x\n", vaddr + i * c_max * type, word[0]);
+			printf("0x%.8x: 0x%.8x\n", vaddr + i * cols_max * type, word[0]);
 			num -= 1;
 		break;
 		case 2:
-			printf("0x%.8x: 0x%.8x 0x%.8x\n", vaddr + i * c_max * type, word[0], word[1]);
+			printf("0x%.8x: 0x%.8x 0x%.8x\n", vaddr + i * cols_max * type, word[0], word[1]);
 			num -= 2;
 		break;
 		case 3:
-			printf("0x%.8x: 0x%.8x 0x%.8x 0x%.8x\n", vaddr + i * c_max * type, word[0], word[1], word[2]);
+			printf("0x%.8x: 0x%.8x 0x%.8x 0x%.8x\n", vaddr + i * cols_max * type, word[0], word[1], word[2]);
 			num -= 3;
 		break;
 		default:
-			printf("0x%.8x: 0x%.8x 0x%.8x 0x%.8x 0x%.8x\n", vaddr + i * c_max * type, word[0], word[1], word[2], word[3]);
+			printf("0x%.8x: 0x%.8x 0x%.8x 0x%.8x 0x%.8x\n", vaddr + i * cols_max * type, word[0], word[1], word[2], word[3]);
 			num -= 4;
 		break;
 		}
