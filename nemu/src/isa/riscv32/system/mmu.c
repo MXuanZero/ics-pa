5a27e75c168bfc751729009070c16bf5aea2a506
tracer-ics2024 <tracer@njuics.org>
2024-10-18 11:01:48 +0800

    >  compile NEMU 231220000 张三 Linux DESKTOP-O70Q5GQ 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:01:48 up  2:08,  1 user,  load average: 0.34, 0.13, 0.04

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -17,32 +17,47 @@
 #include <memory/vaddr.h>
 #include <memory/paddr.h>
 
+static bool isa_is_mem(vaddr_t addr, int type);
+
+static inline bool isa_is_mem(vaddr_t addr, int type)
+{
+  return addr >= PMEM_LEFT && (addr + type) <= PMEM_RIGHT;
+}
+
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
 	uint8_t cols_max = 4; // maximum number of columns
+  vaddr_t addr, read_addr;
 	word_t word[4];
 	int line = len ? ((len - 1) / cols_max) + 1 : 1;
 	int num = len;
 
 	for(int i = 0; i < line; i++) {
-		for(int j = 0; j < 4; j++) {
-			word[j] = vaddr_read(vaddr + i * cols_max + j * type, type);
+    addr = vaddr + i * cols_max * type;
+
+		for(int c = 0; c < 4; c++) {
+      read_addr = addr + c * type;
+      if(isa_is_mem(read_addr, type) == false) {
+        printf("address = 0x%x is out of bound of mem \n", read_addr);
+        return MEM_RET_FAIL;
+      }
+			word[c] = vaddr_read(read_addr, type);
 		}
 
 		switch (num) {
 		case 1:
-			printf("0x%.8x: 0x%.8x\n", vaddr + i * cols_max * type, word[0]);
+			printf("0x%.8x: 0x%.8x\n", addr, word[0]);
 			num -= 1;
 		break;
 		case 2:
-			printf("0x%.8x: 0x%.8x 0x%.8x\n", vaddr + i * cols_max * type, word[0], word[1]);
+			printf("0x%.8x: 0x%.8x 0x%.8x\n", addr, word[0], word[1]);
 			num -= 2;
 		break;
 		case 3:
-			printf("0x%.8x: 0x%.8x 0x%.8x 0x%.8x\n", vaddr + i * cols_max * type, word[0], word[1], word[2]);
+			printf("0x%.8x: 0x%.8x 0x%.8x 0x%.8x\n", addr, word[0], word[1], word[2]);
 			num -= 3;
 		break;
 		default:
-			printf("0x%.8x: 0x%.8x 0x%.8x 0x%.8x 0x%.8x\n", vaddr + i * cols_max * type, word[0], word[1], word[2], word[3]);
+			printf("0x%.8x: 0x%.8x 0x%.8x 0x%.8x 0x%.8x\n", addr, word[0], word[1], word[2], word[3]);
 			num -= 4;
 		break;
 		}
